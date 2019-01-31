6567aaaf0eae51380ae36a77004fe0802b1fb94b
congbing <congbing@everybodycreate.com>
2019-01-31 23:09:12 +0800

    增加数字货币服务提供者

---
+++ src/main/java/com/cb/itoken/web/backend/config/WebBackendInterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.cb.itoken.web.backend.config;
+
+import com.cb.itoken.web.backend.Interceptor.WebBackendInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebBackendInterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    WebBackendInterceptor webBackendInterceptor(){
+        return new WebBackendInterceptor();
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(webBackendInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**");
+    }
+}
